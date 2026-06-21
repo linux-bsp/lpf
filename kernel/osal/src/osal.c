4ebf5b088a59acd65bade07d228b8baa3ea63c20
wanguo <guohaoprc@163.com>
2026-06-21 20:12:11 +0800

    format log

--- kernel/osal/src/osal.c
+++ kernel/osal/src/osal.c
@@ -8,13 +8,20 @@
 void osal_print_version(void)
 {
 	osal_log(OS_LOG_LEVEL_INFO, "OSAL",
-		 "module_version=%u.%u.%u lpf_version=%s git=%s build_time=%s build_by=%s@%s compiler=%s arch=%s kernel=%s",
+		 "================ OSAL version info ================");
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL", "module_version=%u.%u.%u",
 		 OSAL_LITE_VERSION_MAJOR, OSAL_LITE_VERSION_MINOR,
-		 OSAL_LITE_VERSION_PATCH, LPF_VERSION,
-		 LPF_GIT_COMMIT, LPF_COMPILE_TIME,
-		 LPF_COMPILE_BY, LPF_COMPILE_HOST,
-		 LPF_COMPILER, LPF_BUILD_ARCH,
-		 LPF_BUILD_KERNEL);
+		 OSAL_LITE_VERSION_PATCH);
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL", "lpf_version=%s", LPF_VERSION);
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL", "git=%s", LPF_GIT_COMMIT);
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL", "build_time=%s", LPF_COMPILE_TIME);
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL", "build_by=%s@%s",
+		 LPF_COMPILE_BY, LPF_COMPILE_HOST);
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL", "compiler=%s", LPF_COMPILER);
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL", "arch=%s", LPF_BUILD_ARCH);
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL", "kernel=%s", LPF_BUILD_KERNEL);
+	osal_log(OS_LOG_LEVEL_INFO, "OSAL",
+		 "===================================================");
 }
 EXPORT_SYMBOL_GPL(osal_print_version);
 
